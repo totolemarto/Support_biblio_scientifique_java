624b1f15cccd6e9a00e4ee364d8f5b6597a59a8c
Tommy Prats <tommy.prats@etu.univ-grenoble-alpes.fr>
2026-03-18 09:18:16 +0100

    Add the first documentationvery explicit one.

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,6 +4,11 @@ package com.mycompany.app;
  * Hello world!
  */
 public class App {
+    /**
+     * Just a Main function
+     *
+     * @param args  classical args from system
+     */
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
